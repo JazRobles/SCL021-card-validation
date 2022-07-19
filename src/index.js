f2bab584fb86b1c1d8f501f0be16726b6b9e534f
jaz Robles <jazmasoterapeuta@gmail.com>
2022-07-19 02:09:54 -0400

    'segundo/guardar'.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
 import validator from './validator.js';
 
+
+const btnvalidar = document.getElementById('btn');
+
+btnvalidar.addEventListener('click',finvalidacion);
+let guardado = guardado;
+
+function finvalidacion(){
+
+
+
+
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+onload = function(){ 
+  var ele = document.querySelectorAll('.validar')[0];
+  ele.onkeypress = function(e) {
+     if(isNaN(this.value+String.fromCharCode(e.charCode)))
+        return false;
+  }
+  ele.onpaste = function(e){
+     e.preventDefault();
+  }
+}
+
+
+
 console.log(validator);
+
+//valida campo de letras
+
+
+/////////////////////////////////////////////
--- src/validator.js
+++ src/validator.js
@@ -1,4 +1,26 @@
 const validator = {
+  
+    
+  
+  //se crea el onjeto enmascarador 
+  maskify:(creditCardNumber) => {
+    let numerosArray = creditCardNumber.split('');
+    //declaro la variable que contendrá los numeros de la tarjeta transformador en array
+    let point = numerosArray.length - 4
+    //creo una variable que contenga todas las posiciones menos las ultimas 4
+    let numMask = [ ]
+    //creo una variable que contendra los resultados de la iteración del for
+    for (let it = 0; it < point; it++) {
+      numMask.push("#");
+    }
+    //se enmascaran las posiciones del array
+    for (let it = 0; it < 4; it++) {
+      numMask.push(numerosArray[point+it]);
+    }
+    //se agregan los 4 ultimos numeros no enmascarados
+    return numMask.join("")
+  } //junto el resultado de las iteraciones
+  
   // ...
 };
 
